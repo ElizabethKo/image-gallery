22f7b918edab37a042db4557b122cea8e2283057
Elizabeth <krasnajfuria@gmail.com>
2021-09-06 16:22:06 +0300

    clear project

--- src/main.js
+++ src/main.js
@@ -1,23 +1,15 @@
-import Vue from 'vue'
-import Select, {HttpClient} from "@/custom";
-import Gallery from "@/gallery";
-
-require('@/assets/css.css')
-let config = require('./cg.js')
-new Vue({
-    data: {
-        show: true
-    }
-})
+import Select, {HttpClient} from "./custom.js";
+import Gallery from "./gallery.js";
+import {Config} from "./config.js";
 
 const baseSelect = new Select();
-let httpClient = new HttpClient(config['token'])
-httpClient.getCountries('GET', new URL(config['country_url'])).then(value => baseSelect.fill(value))
+let httpClient = new HttpClient(Config['token'])
+httpClient.getCountries('GET', new URL(Config['country_url'])).then(value => baseSelect.fill(value))
 
 
 let selectElement = document.getElementById("name")
     selectElement.addEventListener("change", function () {
-    let url = new URL(config['search_url']);
+    let url = new URL(Config['search_url']);
     url.searchParams.set('query', selectElement.value);
     httpClient.getPhoto("GET", url).then(value => new Gallery().generate(value.photos))
   });
